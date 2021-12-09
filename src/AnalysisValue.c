c378341c94df6980b09530105094f4648f4a73ac
zituryoku0 <tommykouki1031@icloud.com>
2021-12-10 01:03:32 +0900

    まずはお試し感覚 ちょっとTinyPDGは頭おかしい（いい意味で）

---
+++ src/AnalysisValue.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <time.h>
+
+// これがmainになるのか知らんけどとりあえず
+int main ()
+{
+    FILE *fp; // FILE型構造体
+    // 目的ないから、ファイル名指定してるよ〜
+	char fname[] = "test.txt";
+	int chr;
+    double cpuTime;
+ 
+	fp = fopen(fname, "r"); // ファイルを開く。失敗するとNULLを返す。
+	if(fp == NULL) {
+		printf("%s file not open!\n", fname);
+		return -1;
+	}
+ 
+	while((chr = fgetc(fp)) != EOF) {
+		putchar(chr);
+	}
+ 
+    cpuTime = (double)clock();
+    printf("テキストのアドレス：%d\n", chr);
+    printf("CPU時間：%f\n", cpuTime);
+	fclose(fp); // ファイルを閉じる
+ 
+	return 0;
+}
